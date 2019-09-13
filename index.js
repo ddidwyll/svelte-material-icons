4933fb30e827f7db41c4fec4de2375e3d5420f3b
Gray Moss <ddidwyll@gmail.com>
2019-09-13 09:43:00 +0300

    aksljfals

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import ActionDone from './action/Done.svelte'
+import ActionToday from './action/Today.svelte'
 import ActionSearch from './action/Search.svelte'
 import ActionVisibility from './action/Visibility.svelte'
 import ActionVisibilityOff from './action/VisibilityOff.svelte'
-import ActionToday from './action/Today.svelte'
+import ActionAccessibilityNew from './action/AccessibilityNew.svelte'
 
 export {
   ActionDone,
+  ActionToday,
   ActionSearch,
   ActionVisibility,
   ActionVisibilityOff,
-  ActionToday
+  ActionAccessibilityNew
 }
