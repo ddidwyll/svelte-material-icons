b16e7286b86a880c04626e9ce763d5c1dedf6a2b
Gray Moss <ddidwyll@gmail.com>
2019-09-27 10:07:47 +0300

    Add icons

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import ActionDone from './action/Done.svelte'
 import ActionToday from './action/Today.svelte'
 import ActionSearch from './action/Search.svelte'
+import ActionCheckCircleOutline from './action/CheckCircleOutline.svelte'
+import ActionOfflineBolt from './action/OfflineBolt.svelte'
 import ActionVisibility from './action/Visibility.svelte'
 import ActionVisibilityOff from './action/VisibilityOff.svelte'
 import ActionAccessibilityNew from './action/AccessibilityNew.svelte'
@@ -8,6 +10,7 @@ import EditorPostAdd from './editor/PostAdd.svelte'
 import ContentClear from './content/Clear.svelte'
 import ContentAdd from './content/Add.svelte'
 import ContentRemove from './content/Remove.svelte'
+import ContentRemoveCircleOutline from './content/RemoveCircleOutline.svelte'
 import ContentBlock from './content/Block.svelte'
 import NotificationEventBusy from './notification/EventBusy.svelte'
 import NotificationEventAvailable from './notification/EventAvailable.svelte'
@@ -22,34 +25,43 @@ import SocialOutdoorGrill from './social/OutdoorGrill.svelte'
 import HardwareKeyboardArrowDown from './hardware/KeyboardArrowDown.svelte'
 import HardwareKeyboardArrowUp from './hardware/KeyboardArrowUp.svelte'
 import ImageEdit from './image/Edit.svelte'
+import ImagePanoramaFishEye from './image/PanoramaFishEye.svelte'
 import ToggleCheckBox from './toggle/CheckBox.svelte'
 import ToggleCheckBoxOutlineBlank from './toggle/CheckBoxOutlineBlanck.svelte'
+import AvPauseCircleOutline from './av/PauseCircleOutline.svelte'
+import AvPlayCircleOutline from './av/PlayCircleOutline.svelte'
 
 export {
   ActionDone,
   ActionToday,
   ActionSearch,
+  ActionOfflineBolt,
   ActionVisibility,
   ActionVisibilityOff,
   ActionAccessibilityNew,
+  ActionCheckCircleOutline,
   EditorPostAdd,
-  ContentClear,
   ContentAdd,
-  ContentRemove,
   ContentBlock,
-  NotificationEventBusy,
+  ContentClear,
+  ContentRemove,
+  ContentRemoveCircleOutline,
   NotificationEventAvailable,
+  NotificationEventBusy,
   MapsLocalAtm,
-  MapsLocalPrintshop,
   MapsRestaurantMenu,
+  MapsLocalPrintshop,
   PlacesKitchen,
   PlacesRoomService,
   CommunicationListAlt,
   SocialDeck,
   SocialOutdoorGrill,
-  HardwareKeyboardArrowDown,
   HardwareKeyboardArrowUp,
+  HardwareKeyboardArrowDown,
   ImageEdit,
+  ImagePanoramaFishEye,
   ToggleCheckBox,
-  ToggleCheckBoxOutlineBlank
+  ToggleCheckBoxOutlineBlank,
+  AvPauseCircleOutline,
+  AvPlayCircleOutline
 }
