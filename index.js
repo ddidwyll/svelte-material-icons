92f46c479fff21ceaecfacd3cf6c0d0ff290fc6d
Gray Moss <ddidwyll@gmail.com>
2019-09-30 09:51:34 +0300

    Add icons

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 import ActionDone from './action/Done.svelte'
 import ActionToday from './action/Today.svelte'
 import ActionSearch from './action/Search.svelte'
+import ActionDoneAll from './action/DoneAll.svelte'
+import ActionHighlightOff from './action/HighlightOff.svelte'
 import ActionCheckCircleOutline from './action/CheckCircleOutline.svelte'
+import ActionAccessibilityNew from './action/AccessibilityNew.svelte'
+import ActionVisibilityOff from './action/VisibilityOff.svelte'
 import ActionOfflineBolt from './action/OfflineBolt.svelte'
 import ActionVisibility from './action/Visibility.svelte'
-import ActionVisibilityOff from './action/VisibilityOff.svelte'
-import ActionAccessibilityNew from './action/AccessibilityNew.svelte'
 import EditorPostAdd from './editor/PostAdd.svelte'
-import ContentClear from './content/Clear.svelte'
 import ContentAdd from './content/Add.svelte'
+import ContentClear from './content/Clear.svelte'
+import ContentBlock from './content/Block.svelte'
 import ContentRemove from './content/Remove.svelte'
 import ContentRemoveCircleOutline from './content/RemoveCircleOutline.svelte'
-import ContentBlock from './content/Block.svelte'
-import NotificationEventBusy from './notification/EventBusy.svelte'
 import NotificationEventAvailable from './notification/EventAvailable.svelte'
+import NotificationEventBusy from './notification/EventBusy.svelte'
 import MapsLocalAtm from './maps/LocalAtm.svelte'
 import MapsLocalPrintshop from './maps/LocalPrintshop.svelte'
 import MapsRestaurantMenu from './maps/RestaurantMenu.svelte'
@@ -26,8 +28,8 @@ import HardwareKeyboardArrowDown from './hardware/KeyboardArrowDown.svelte'
 import HardwareKeyboardArrowUp from './hardware/KeyboardArrowUp.svelte'
 import ImageEdit from './image/Edit.svelte'
 import ImagePanoramaFishEye from './image/PanoramaFishEye.svelte'
-import ToggleCheckBox from './toggle/CheckBox.svelte'
 import ToggleCheckBoxOutlineBlank from './toggle/CheckBoxOutlineBlanck.svelte'
+import ToggleCheckBox from './toggle/CheckBox.svelte'
 import AvPauseCircleOutline from './av/PauseCircleOutline.svelte'
 import AvPlayCircleOutline from './av/PlayCircleOutline.svelte'
 
@@ -35,8 +37,10 @@ export {
   ActionDone,
   ActionToday,
   ActionSearch,
-  ActionOfflineBolt,
+  ActionDoneAll,
   ActionVisibility,
+  ActionOfflineBolt,
+  ActionHighlightOff,
   ActionVisibilityOff,
   ActionAccessibilityNew,
   ActionCheckCircleOutline,
