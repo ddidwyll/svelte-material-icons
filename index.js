fea133789b275e538e5be516ad36583a31d8e870
Gray Moss <ddidwyll@gmail.com>
2019-10-11 15:19:21 +0300

    fsduyf

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 import ActionDone from './action/Done.svelte'
+import ActionPrint from './action/Print.svelte'
 import ActionToday from './action/Today.svelte'
 import ActionSearch from './action/Search.svelte'
 import ActionDoneAll from './action/DoneAll.svelte'
+import ActionCreditCard from './action/CreditCard.svelte'
 import ActionHighlightOff from './action/HighlightOff.svelte'
+import ActionDeleteForever from './action/DeleteForever.svelte'
+import ActionSettingsApplications from './action/SettingsApplications.svelte'
 import ActionCheckCircleOutline from './action/CheckCircleOutline.svelte'
 import ActionAccessibilityNew from './action/AccessibilityNew.svelte'
 import ActionVisibilityOff from './action/VisibilityOff.svelte'
 import ActionOfflineBolt from './action/OfflineBolt.svelte'
 import ActionVisibility from './action/Visibility.svelte'
 import EditorPostAdd from './editor/PostAdd.svelte'
+import EditorMonetizationOn from './editor/MonetizationOn.svelte'
 import ContentAdd from './content/Add.svelte'
 import ContentClear from './content/Clear.svelte'
 import ContentBlock from './content/Block.svelte'
@@ -36,16 +41,21 @@ import AvPlayCircleOutline from './av/PlayCircleOutline.svelte'
 
 export {
   ActionDone,
+  ActionPrint,
   ActionToday,
   ActionSearch,
   ActionDoneAll,
+  ActionCreditCard,
   ActionVisibility,
   ActionOfflineBolt,
   ActionHighlightOff,
+  ActionDeleteForever,
   ActionVisibilityOff,
   ActionAccessibilityNew,
   ActionCheckCircleOutline,
+  ActionSettingsApplications,
   EditorPostAdd,
+  EditorMonetizationOn,
   ContentAdd,
   ContentBlock,
   ContentClear,
